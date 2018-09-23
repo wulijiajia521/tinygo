aa2bea57c5cc6ebab115ea571ed0a93ced63da4d
Ayke van Laethem <aykevanlaethem@gmail.com>
2018-09-23 23:31:28 +0200

    avr: add interrupt support
    Interrupts are supported using a special //go:interrupt pragma.
    For example:
    
    //go:interrupt INT0_vect
    func handleINT0() {
        // do something here
    }

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -1171,6 +1171,9 @@ func (c *Compiler) parseFunc(frame *Frame) error {
 	if !frame.fn.IsExported() {
 		frame.fn.LLVMFn.SetLinkage(llvm.InternalLinkage)
 	}
+	if frame.fn.IsInterrupt() && strings.HasPrefix(c.triple, "avr") {
+		frame.fn.LLVMFn.SetFunctionCallConv(85) // CallingConv::AVR_SIGNAL
+	}
 
 	if c.debug {
 		pos := c.ir.Program.Fset.Position(frame.fn.Pos())
