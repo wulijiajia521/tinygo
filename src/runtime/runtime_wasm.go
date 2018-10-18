5d87aedb49773367ff60bf7ac4fd17f61ffeefbe
Ayke van Laethem <aykevanlaethem@gmail.com>
2018-10-18 15:15:29 +0200

    wasm: WIP

---
+++ src/runtime/runtime_wasm.go
@@ -0,0 +1,41 @@
+// +build wasm,!arm,!avr
+
+package runtime
+
+type timeUnit int64
+
+const tickMicros = 1
+
+var timestamp timeUnit
+
+var line []byte
+
+func putchar(c byte) {
+	switch c {
+	case '\r':
+		// ignore
+	case '\n':
+		// write line
+		line = line[:0]
+	default:
+		line = append(line, c)
+	}
+}
+
+func sleepTicks(d timeUnit) {
+	// TODO: actually sleep here for the given time.
+	timestamp += d
+}
+
+func ticks() timeUnit {
+	return timestamp
+}
+
+// Align on word boundary.
+func align(ptr uintptr) uintptr {
+	return (ptr + 3) &^ 3
+}
+
+func abort() {
+	// TODO
+}
